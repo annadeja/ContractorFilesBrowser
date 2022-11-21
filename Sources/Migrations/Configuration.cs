12eacf4d7a6a03466623e95ce8fc9d32740b702f
Laxe <houlipatch@gmail.com>
2022-11-21 01:43:08 +0100

    Migration cleanup.

--- Sources/Migrations/Configuration.cs
+++ Sources/Migrations/Configuration.cs
@@ -21,39 +21,7 @@
 
         protected override void Seed(ContractorFilesBrowser.Data.ContractorContext context)
         {
-            base.Seed(context);
-            Assembly assembly = Assembly.GetExecutingAssembly();
-            string resourceName = "ContractorFilesBrowser.Data.SeederFiles.Countries.csv";
-            using (Stream stream = assembly.GetManifestResourceStream(resourceName))
-            {
-                using (StreamReader reader = new StreamReader(stream, Encoding.UTF8))
-                {
-                    CsvReader csvReader = new CsvReader(reader, CultureInfo.InvariantCulture);
-                    //csvReader.Configuration.WillThrowOnMissingField = false;
-                    var countries = csvReader.GetRecords<Country>().ToArray();
-                    context.Countries.AddOrUpdate(countries);
-                }
-            }
-
-            resourceName = "ContractorFilesBrowser.Data.SeederFiles.Provinces.csv";
-            using (Stream stream = assembly.GetManifestResourceStream(resourceName))
-            {
-                using (StreamReader reader = new StreamReader(stream, Encoding.UTF8))
-                {
-                    CsvReader csvReader = new CsvReader(reader, CultureInfo.InvariantCulture);
-                    //csvReader.Configuration. = false;
-                    //while (csvReader.Read())
-                    {
-                        //var provinceState = csvReader.GetRecord<Province>();
-                        //var countryCode = csvReader.GetField<string>("CountryCode");
-                        //context.Provinces.AddOrUpdate(p => p.Code, provinceState);
-                    }
-                    var provinces = csvReader.GetRecords<Province>().ToArray();
-                    context.Provinces.AddOrUpdate(provinces);
-                }
-            }
-
-            context.SaveChanges();
+            
         }
     }
 }
